7556f5c489aa6679821c3a32381e704cea5cc8c3
lcy <824963750@qq.com>
2016-07-22 23:15:51 +0800

    Feature1 (#2)
    * reset the project

---
+++ src/com/qu7bicycle/dao/AbstractBaseDao.java
@@ -0,0 +1,68 @@
+package com.qu7bicycle.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.Resource;
+
+import com.qu7bicycle.common.GetEntityClassUtil;
+
+public abstract class AbstractBaseDao<T, PK extends Serializable> implements
+		BaseDao<T, PK> {
+	
+	@Resource
+	private Idao<T, PK> iDao;
+	@Resource(name="pageDao")
+	private PageDaoImpl pageDao;
+	
+	
+	
+	public PageDaoImpl getPageDao() {
+		return pageDao;
+	}
+
+	public void setPageDao(PageDaoImpl pageDao) {
+		this.pageDao = pageDao;
+	}
+	
+	public Class<T> getEntityClass() {
+		return entityClass;
+	}
+
+	public void setEntityClass(Class<T> entityClass) {
+		this.entityClass = entityClass;
+	}
+
+	private Class<T> entityClass = GetEntityClassUtil.getEntityClass(getClass());
+	public void save(T entity) {
+		iDao.save(entity);
+	}
+	
+	public List<T> findByHql(String hql){
+		return iDao.findByHql(hql);
+	}
+
+	public void delete(PK... pks) {
+		iDao.delete(entityClass, pks);
+	}
+
+	public T findById(PK pk) {
+		return iDao.findById(entityClass, pk);
+	}
+
+	public List<T> findAll() {
+		return iDao.findAll(entityClass);
+	}
+
+	public void update(T entity) {
+		iDao.update(entity);
+	}
+
+	public Idao<T, PK> getiDao() {
+		return iDao;
+	}
+
+	public void setiDao(Idao<T, PK> iDao) {
+		this.iDao = iDao;
+	}
+
+}
--- src/com/rightsallocate/service/RoleService.java
+++
@@ -1,8 +0,0 @@
-package com.rightsallocate.service;
-
-import com.rightsallocate.dao.BaseDao;
-import com.rightsallocate.model.Role;
-
-public interface RoleService extends BaseDao<Role, String> {
-
-}
